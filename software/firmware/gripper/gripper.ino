093de32e1f80340f03ab403f5d31c83ae185c75b
Vinayaka Patrick Thompson <vinayaka.p.thompson@live.com>
2019-05-25 16:27:22 -0700

    all functions + generalized Ave prog implemented

--- software/firmware/gripper/gripper.ino
+++ software/firmware/gripper/gripper.ino
@@ -3,12 +3,54 @@
 #define AMPMAX 4 // probably can take 6 but 4 is good for safety.
 #define FORWARD 1 //pinch = forward  open = !forward
 
-bool homed = false;
-float speed = 0; //0-100
-float speedLimit=0;
+
+
 
 //function stubs
+void sensorUpdate(); //used to update all internal sensor values
+void getmodBusUpdate();
 bool home(); // used to home the gripper moter
+void limitUpdate();
+void moveMotor(); 
+
+
+// average class discriber
+template <typename T> 
+class Average
+{
+public:
+  Average(int arraySize); // made to handle possible error case
+
+  T update(T);
+  bool full();
+
+  // library-accessible "private" interface
+private:
+  T *myQueue;
+  int size = 0;
+  int length;
+  void enqueue(T);
+  T queueAve();
+};
+
+
+
+//general settings
+bool ishomed = false;
+float speed = 0; //0-100?
+
+//sensor variables
+float currentAMPS = 0;
+
+//speed limit variables
+float currentSpeedLimit=0;
+
+//homing variables
+Average<float> ampAverage = Average<float>(20); //Used in homing function
+Average<float> localAmpAverage = Average<float>(4);
+
+
+
 
 void setup() {
   // Pin Setup:
@@ -16,34 +58,117 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   sensorUpdate();
-
-  if(homed)
+  getmodBusUpdate(); //canU
+  
+  if(ishomed)
   {
-    homed = home();
+    ishomed = home();
   }
-  else
-  {
-    
-  }
+  limitUpdate(); //possibly Average
+  moveMotor(); 
+  
   
 
 }
 
-void sensorUpdate();
+void sensorUpdate()
+{
+
+  return;
+}
+
+void getmodBusUpdate()
+{
+
+  return;
+}
 
 bool home()
 {
-  // four volt shut off
-  speed
+  bool homed = false;
 
+  float averageAmps = 0.0;
+  float localAverage = 0.0;
+  
+  averageAmps = ampAverage.update(currentAMPS);
+  localAverage = localAmpAverage.update(currentAMPS);
 
-  return 1;
+  if(true == ampAverage.full())
+  {
+    speed;
+
+  }
+  
+  return homed;
 }
 
-float limitUpdate()
+void limitUpdate()
 {
+  return;
+}
+void moveMotor()
+{
+   return;
+}
 
-  return speedLimit
+
+
+
+
+
+
+
+
+
+//should probably be broken out into a library
+
+
+
+template <typename T> 
+Average<T>::Average(int arraySize)
+{
+  size = arraySize;
+
+  myQueue[size] = {-1.0} ;
+  length = size;
+}
+
+template <typename T> 
+T Average<T>::update(T number)
+{
+  enqueue(number);
+  return queueAve();
+}
+
+template <typename T>
+bool Average<T>::full()
+{
+  bool isFull = false;
+  
+  if (size<length)
+    isFull = true;
+  
+  return isFull;
+}
+
+
+// Private Methods 
+template <typename T>
+void Average<T>::enqueue(T newNumber)
+{
+  myQueue[length%size] = newNumber;
+  length++;
+}
+
+template <typename T>
+T Average<T>::queueAve()
+{
+  T result = 0;
+  for (int i = 0; size > i; i++)
+  {
+    if((-1.0) != myQueue[i])
+      result += myQueue[i];
+  }
+  return (result/size);
 }
