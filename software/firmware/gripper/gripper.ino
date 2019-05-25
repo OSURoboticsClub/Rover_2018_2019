7e5b962740a8b1360d912bfb38dd7a442df77e63
Vinayaka Patrick Thompson <vinayaka.p.thompson@live.com>
2019-05-25 14:42:14 -0700

    created many vars needed for homeing code

--- software/firmware/gripper/gripper.ino
+++ software/firmware/gripper/gripper.ino
@@ -1,23 +1,49 @@
 //Pinouts
 //Settings
 #define AMPMAX 4 // probably can take 6 but 4 is good for safety.
-//current sense
+#define FORWARD 1 //pinch = forward  open = !forward
+
+bool homed = false;
+float speed = 0; //0-100
+float speedLimit=0;
 
 //function stubs
-bool home();
+bool home(); // used to home the gripper moter
 
 void setup() {
-  // put your setup code here, to run once:
+  // Pin Setup:
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  sensorUpdate();
+
+  if(homed)
+  {
+    homed = home();
+  }
+  else
+  {
+    
+  }
+  
 
 }
 
+void sensorUpdate();
+
 bool home()
 {
+  // four volt shut off
+  speed
 
 
+  return 1;
+}
+
+float limitUpdate()
+{
+
+  return speedLimit
 }
